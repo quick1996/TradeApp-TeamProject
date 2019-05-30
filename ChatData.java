c1193cf5779867d3f8cc0d266fc0d61ce861dc6c
jun130 <38622982+jun130@users.noreply.github.com>
2019-05-30 20:09:58 +0900

    Add files via upload
    asdasd

---
+++ ChatData.java
@@ -0,0 +1,40 @@
+package com.example.myapplication;
+
+public class ChatData {
+    private String userName;
+    private String message;
+    private String time;
+
+    public ChatData() {
+    }
+
+    public ChatData(String userName, String message,String time) {
+        this.userName = userName;
+        this.message = message;
+        this.time = time;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
